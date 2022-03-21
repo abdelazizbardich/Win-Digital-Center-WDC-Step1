6716719f3918bf458dfbdb446471b4c3fd0f7b1c
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-03-21 16:36:36 +0100

    Update db_script.sql

--- db_script.sql
+++ db_script.sql
@@ -1,87 +1,92 @@
-create table if not exists Admin
+create table exercises
 (
-    id           int auto_increment
-        primary key,
-    firstname    varchar(20)  null,
-    lastname     varchar(20)  null,
-    login        varchar(20)  null,
-    password     varchar(300) null,
-    phone_number varchar(15)  null,
-    email        varchar(50)  null,
-    status       tinyint(1)   null
+    exercise_id bigintnot null primary key,
+    end_date    datetime     null,
+    start_date  datetime     null,
+    status      int          null,
+    title       varchar(255) null,
+    year        int          null
 );
 
-create table if not exists Exercice
+create table activities
 (
-    id         int auto_increment
-        primary key,
-    title      varchar(100) null,
-    year       int          null,
-    status     varchar(20)  null,
-    start_date datetime     null,
-    end_date   datetime     null,
-    constraint CHK_ExerciceStatus
-        check (`status` in (_utf8mb4\'InProgress\',_utf8mb4\'Completed\'))
+    activity_id bigintnot null primary key,
+    description varchar(255) null,
+    end_date    datetime     null,
+    start_date  datetime     null,
+    status      int          null,
+    title       varchar(255) null,
+    type        int          null,
+    exercise_id bigint       null,
+    constraint FK3sh39x4up3miyhby7op2fldca
+        foreign key (exercise_id) references exercises (exercise_id)
 );
 
-create table if not exists Participant
+create table responsible_types
 (
-    id           int auto_increment
-        primary key,
-    firstname    varchar(30) null,
-    lastname     varchar(20) null,
-    phone_number varchar(15) null,
-    email        varchar(50) null,
-    domain       varchar(30) null,
-    structure    varchar(50) null
+    responsible_type_id bigintnot null primary key,
+    type                varchar(255) null
 );
 
-create table if not exists Responsible
+create table roles
 (
-    id           int auto_increment
-        primary key,
-    firstname    varchar(20) null,
-    lastname     varchar(20) null,
-    phone_number varchar(15) null,
-    domain       varchar(20) null,
-    type         varchar(20) null,
-    constraint CHK_ResponsibleType
-        check (`type` in (_utf8mb4\'Formateur\',_utf8mb4\'Intervenant\'))
+    role_id bigintnot null primary key,
+    access  varchar(255) null,
+    name    varchar(255) null
 );
 
-create table if not exists Activity
+create table users
 (
-    id             int auto_increment
-        primary key,
-    title          varchar(100) null,
-    description    varchar(200) null,
-    type           varchar(20)  null,
-    status         varchar(20)  null,
-    end_date       datetime     null,
-    start_date     datetime     null,
-    exercice_id    int          not null,
-    responsible_id int          not null,
-    constraint Activity_Exercice_id_fk
-        foreign key (exercice_id) references Exercice (id)
-            on update cascade on delete cascade,
-    constraint Activity_Responsible_id_fk
-        foreign key (responsible_id) references Responsible (id)
-            on update cascade on delete cascade,
-    constraint CHK_ActivityStatus
-        check (`status` in (_utf8mb4\'Enabled\',_utf8mb4\'Disabled\')),
-	constraint CHK_ActivityType
-		check (`type` in (_utf8mb4\'Formateur\',_utf8mb4\'Talk\',_utf8mb4\'Event\'))
+    dtype               varchar(31)  not null,
+    user_id             bigintnot null primary key,
+    email               varchar(255) null,
+    first_name          varchar(255) null,
+    last_name           varchar(255) null,
+    login               varchar(255) null,
+    password            varchar(255) null,
+    phone_number        varchar(255) null,
+    status              bit          null,
+    domain              varchar(255) null,
+    structure           varchar(255) null,
+    role_id             bigint       null,
+    responsible_type_id bigint       null,
+    constraint FKj8qpdkdtc8pw024sl0hr7c5ni
+        foreign key (responsible_type_id) references responsible_types (responsible_type_id),
+    constraint FKp56c1712k691lhsyewcssf40f
+        foreign key (role_id) references roles (role_id)
 );
 
-create table if not exists Participate
+create table participants
 (
-    attendance     tinyint(1) null,
-    activity_id    int        not null,
-    participant_id int        not null,
-    constraint Participate_Activity_id_fk
-        foreign key (activity_id) references Activity (id)
-            on update cascade on delete cascade,
-    constraint Participate_Participant_id_fk
-        foreign key (participant_id) references Participant (id)
-            on update cascade on delete cascade
+    domain    varchar(255) null,
+    structure varchar(255) null,
+    user_id   bigintnot null primary key,
+    constraint FKghixrahoj1s8cloinfx8lyeqa
+        foreign key (user_id) references users (user_id)
 );
+
+create table participates
+(
+    participate_id bigint not null primary key,
+    attendance     bit    null,
+    activity_id    bigint null,
+    participant_id bigint null,
+    constraint FKck52ts3qtmhd6dkrtpe8d1d7m
+        foreign key (activity_id) references activities (activity_id),
+    constraint FKebm4ppnxypju3eq4s53tb9u2c
+        foreign key (participant_id) references users (user_id),
+    constraint FKqlw115w817ws8lnn87k83lsnq
+        foreign key (participant_id) references participants (user_id)
+);
+
+create table responsibles
+(
+    domain              varchar(255) null,
+    user_id             bigintnot null primary key,
+    responsible_type_id bigint       null,
+    constraint FKqxayxkk64dk4rx5i9sawlraak
+        foreign key (responsible_type_id) references responsible_types (responsible_type_id),
+    constraint FKrsouwttyl5ycf16jr0pgdl17m
+        foreign key (user_id) references users (user_id)
+);
+
